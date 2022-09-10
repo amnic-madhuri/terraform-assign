19e29e13ab5818dccd31908c4aff0e9fab86d0c1
amnic-madhuri <107679119+amnic-madhuri@users.noreply.github.com>
2022-09-11 00:35:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 # vpc.tf | VPC Configuration
 
 resource "aws_vpc" "aws-vpc" {
-  cidr_block           = "${vpc.cidr}"
+  cidr_block           = "${var.cidr}"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
